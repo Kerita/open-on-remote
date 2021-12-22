3128478c42dc4cf91d680c72c1bcb4f5a664d98b
Kerita <kerita@qq.com>
2021-12-22 20:16:40 +0800

    feat: support http mode

--- build/index.js
+++ build/index.js
@@ -5,23 +5,28 @@ var cp = require("child_process");
 var git = function (args) {
     return cp.spawnSync('git', args);
 };
-var commandForRemoteUrl = git(['config', '--get', 'remote.origin.url']);
-var remoteUrl = commandForRemoteUrl.stdout.toString().trim();
-if (!remoteUrl) {
-    throw new Error('Could not find remote url');
+function openOnRemote() {
+    var commandForRemoteUrl = git(['config', '--get', 'remote.origin.url']);
+    var remoteUrl = commandForRemoteUrl.stdout.toString().trim();
+    if (!remoteUrl) {
+        throw new Error('Could not find remote url');
+    }
+    var commandForCurrentBranch = git(['rev-parse', '--abbrev-ref', 'HEAD']);
+    var currentBranch = commandForCurrentBranch.stdout.toString().trim();
+    if (!currentBranch) {
+        throw new Error('Could not find current branch');
+    }
+    // support SSH mode and HTTP mode
+    var remoteUrlRegExp = /^(git@|https:\/\/)(.*).git$/;
+    var matchResult = remoteUrl.match(remoteUrlRegExp);
+    var repositoryUrl = (matchResult === null || matchResult === void 0 ? void 0 : matchResult.length) ? matchResult[2] : '';
+    if (!repositoryUrl) {
+        throw new Error('Could not find repository url');
+    }
+    var completedUrl = "https://" + repositoryUrl.replace(':', '/') + "/tree/" + currentBranch;
+    // eslint-disable-next-line no-console
+    console.log('Open on remote in default browser: \n', completedUrl);
+    cp.spawn('open', [completedUrl]);
 }
-var commandForCurrentBranch = git(['rev-parse', '--abbrev-ref', 'HEAD']);
-var currentBranch = commandForCurrentBranch.stdout.toString().trim();
-if (!currentBranch) {
-    throw new Error('Could not find current branch');
-}
-var remoteUrlRegExp = /^git@(.*).git$/;
-var matchResult = remoteUrl.match(remoteUrlRegExp);
-var repositoryUrl = (matchResult === null || matchResult === void 0 ? void 0 : matchResult.length) ? matchResult[1] : '';
-if (!repositoryUrl) {
-    throw new Error('Could not find repository url');
-}
-var completedUrl = "https://" + repositoryUrl.replace(':', '/') + "/tree/" + currentBranch;
-// eslint-disable-next-line no-console
-console.log('Open on remote in default browser: \n', completedUrl);
-cp.spawn('open', [completedUrl]);
+exports.default = openOnRemote;
+openOnRemote();
--- src/index.ts
+++ src/index.ts
@@ -5,40 +5,40 @@ import cp = require('child_process');
 const git = (args: string[]): cp.SpawnSyncReturns<string> =>
   cp.spawnSync('git', args);
 
-const commandForRemoteUrl = git(['config', '--get', 'remote.origin.url']);
-const remoteUrl = commandForRemoteUrl.stdout.toString().trim();
+export default function openOnRemote() {
+  const commandForRemoteUrl = git(['config', '--get', 'remote.origin.url']);
+  const remoteUrl = commandForRemoteUrl.stdout.toString().trim();
 
-if (!remoteUrl) {
-  throw new Error('Could not find remote url');
+  if (!remoteUrl) {
+    throw new Error('Could not find remote url');
+  }
+
+  const commandForCurrentBranch = git(['rev-parse', '--abbrev-ref', 'HEAD']);
+
+  const currentBranch = commandForCurrentBranch.stdout.toString().trim();
+
+  if (!currentBranch) {
+    throw new Error('Could not find current branch');
+  }
+
+  // support SSH mode and HTTP mode
+  const remoteUrlRegExp = /^(git@|https:\/\/)(.*).git$/;
+  const matchResult = remoteUrl.match(remoteUrlRegExp);
+  const repositoryUrl = matchResult?.length ? matchResult[2] : '';
+
+  if (!repositoryUrl) {
+    throw new Error('Could not find repository url');
+  }
+
+  const completedUrl = `https://${repositoryUrl.replace(
+    ':',
+    '/',
+  )}/tree/${currentBranch}`;
+
+  // eslint-disable-next-line no-console
+  console.log('Open on remote in default browser: \n', completedUrl);
+
+  cp.spawn('open', [completedUrl]);
 }
 
-const commandForCurrentBranch = git(['rev-parse', '--abbrev-ref', 'HEAD']);
-
-const currentBranch = commandForCurrentBranch.stdout.toString().trim();
-
-if (!currentBranch) {
-  throw new Error('Could not find current branch');
-}
-
-// support SSH mode and HTTP mode
-const remoteUrlRegExp = /^(git@|https:\/\/)(.*).git$/;
-const matchResult = remoteUrl.match(remoteUrlRegExp);
-const repositoryUrl = matchResult?.length ? matchResult[2] : '';
-
-console.log('kerita log:', matchResult, 'matchResult');
-
-if (!repositoryUrl) {
-  throw new Error('Could not find repository url');
-}
-
-console.log('kerita log:', repositoryUrl, 'repositoryUrl');
-
-const completedUrl = `https://${repositoryUrl.replace(
-  ':',
-  '/',
-)}/tree/${currentBranch}`;
-
-// eslint-disable-next-line no-console
-console.log('Open on remote in default browser: \n', completedUrl);
-
-cp.spawn('open', [completedUrl]);
+openOnRemote();
