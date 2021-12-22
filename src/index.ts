4e1b4422186811485f2412a89dc85fbc6a1d17cc
Kerita <kerita@qq.com>
2021-12-22 16:46:32 +0800

    feat: add git command to get remote url & current branch

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+import cp = require('child_process');
+
+const git = (args: string[]): cp.SpawnSyncReturns<string> =>
+  cp.spawnSync('git', args);
+
+const commandForRemoteUrl = git(['config', '--get', 'remote.origin.url']);
+const remoteUrl = commandForRemoteUrl.stdout.toString().trim();
+
+if (!remoteUrl) {
+  throw new Error('Could not find remote url');
+}
+
+const commandForCurrentBranch = git(['rev-parse', '--abbrev-ref', 'HEAD']);
+
+const currentBranch = commandForCurrentBranch.stdout.toString().trim();
+if (!currentBranch) {
+  throw new Error('Could not find current branch');
+}
